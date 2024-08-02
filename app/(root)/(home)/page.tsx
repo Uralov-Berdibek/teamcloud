8c550eb2ddcd5da73556cac41b8e59f612693be0
berdibek-2002 <uralov0212@gmail.com>
2024-08-02 11:33:12 +0500

    version 1.0.9

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -195,7 +195,7 @@ const Home = () => {
                   lorem, vulputate at sapien sit amet, auctor iaculis lorem.
                 </p>
 
-                <Link href='/auth' className='btnh btn-primary-h'>
+                <Link href='/auth' className='btnh btn-primary-h text-center'>
                   Get Started
                 </Link>
               </div>
