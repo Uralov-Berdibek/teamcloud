741037698ea9305fbfd5d02d4bbe6bd3939a7420
berdibek-2002 <uralov0212@gmail.com>
2024-08-10 12:06:37 +0500

    version clear

--- app/(root)/site/layout.tsx
+++ app/(root)/site/layout.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import Header from '../../../components/global/header';
-import Sidebar from '../../../components/global/sidebar';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='flex h-screen bg-gray-100'>
-      <Sidebar />
-      <main className='flex-1 p-6'>
-        <Header />
-        {children}
-      </main>
+      <main className='flex-1 p-6'>{children}</main>
     </div>
   );
 };
--- app/(root)/teams/[username]/page.tsx
+++ app/(root)/teams/[username]/page.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import AllSites from '../../../../components/shared/allsites';
-import RecentSites from '../../../../components/shared/recent-sites';
 
 const Home = ({ params }: { params: { username: string } }) => {
-  return (
-    <div>
-      <AllSites />
-      <RecentSites />
-    </div>
-  );
+  return <div>{params.username}</div>;
 };
 
 export default Home;
